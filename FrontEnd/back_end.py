bc5537439c6ebeffe2b72a22c2b751e9114016da
Eshaan0112 <eminocha@purdue.edu>
2023-04-23 12:42:58 -0400

    init

---
+++ FrontEnd/back_end.py
@@ -0,0 +1,60 @@
+# Import Statements
+import os
+import sys
+import time
+sys.path.append("../Backend")
+from trendFollowing import *
+# print('Installing Packages')
+# os.system('pip install -r requirements.txt')
+
+import json
+from flask import Flask, render_template, request, jsonify,redirect, url_for
+from flask_cors import CORS
+app = Flask(__name__)
+
+CORS(app)
+
+'''
+This function handles the POST request to the /upload endpoint 
+and reads the contents of the uploaded file using Flask's request.files object
+'''
+
+@app.route("/upload", methods=['POST'])
+def upload_file():
+    selected_strings = request.form.getlist('strings[]')
+    if len(selected_strings) == 0:
+        return {'status': 'failure'}
+
+    print(f'Selected Strings: {selected_strings}')
+
+    # Call simple algorithm - Read jsons
+    start_time = time.time()
+
+    print("Shifting Directories")
+    os.chdir('../Backend')
+    JsonList = main_trendFollowing(selected_strings)
+    json_returned = {'status':'success', 'JsonList': JsonList}
+    os.chdir('../FrontEnd')
+    # Writing to a stdout file
+    # with open('output_file.txt', 'wb') as output_file:
+    #     # Write the contents of the input file to the new file
+    #     output_file.write(content.encode('utf-8'))
+
+    # print("Flushed output to 'output_file.txt' file")
+    return json_returned
+
+# @app.route("/results")
+# def results():
+#     # retrieve the results from the query parameters
+#     results = json.loads(request.args.get('data'))
+#     # render the "results.html" template with the results as context
+#     return render_template("results.html", results=results)
+
+if __name__ == "__main__":
+    with open("config.json") as f:
+        config = json.load(f)
+
+    port = config["port"]
+    print(f'App running on port {port}')
+    app.run(host='localhost', port=port)
+    
---
+++ data_collector.py
@@ -0,0 +1,24 @@
+# Import Statements
+import csv
+import urllib.request as urllib2
+import codecs
+from urllib.parse import urlencode, urljoin, urlparse, quote
+from pandas import *
+
+stock_csv = read_csv('Stocks in the SP 500 Index.csv')
+
+stock = stock_csv['Symbol'].tolist()
+
+key = '63dbd073810b06.72223265' # Trevor's key
+for i in stock:
+    url = "https://eodhistoricaldata.com/api/intraday/%s.US?api_token=%s&interval=1m" % (i, key) 
+    req = urllib2.Request(url)
+    req.add_header('User-Agent','Mozilla/5.0')
+    response = urllib2.urlopen(req)
+
+    cr = csv.reader(codecs.iterdecode(response, 'utf-8')) 
+    fptr = open('test/%s.csv' % (i), 'w', newline ='') 
+
+    for row in cr:
+        writer = csv.writer(fptr, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL) 
+        writer.writerow(row)
